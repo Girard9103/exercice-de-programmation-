57b825236f05480afca4c367d3718620694344db
Girard <girard1803@hotmail.fr>
2019-07-19 18:21:59 +0200

    struturation

---
+++ exercise N°1.cs
@@ -0,0 +1,22 @@
+
+using System;
+
+public class Exercise
+{   
+    public void fonctionprincipale()
+{
+//Exercise N° 1 
+string nomUtilisateur;
+console.Write("Entrez  votre Nom : ");
+nomUtilisateur = Utilisateur.saisirTexte();
+console.Writeline("Bonjour" + nomUtilisateur + "!");
+
+//Exercise N°2
+
+
+
+}
+
+    
+}
+
